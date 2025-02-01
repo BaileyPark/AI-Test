27fa4b23d01e61639e125d2bda118ce76a275eaf
bailey <baileypark@kakao.com>
2025-02-01 18:59:16 +0900

    fix: ChatUpstage 대신 SolarChat를 사용

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain_upstage import ChatUpstage
+from langchain_community.chat_models.solar import SolarChat # ✅ 최신 버전에서 올바른 경로
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage
 st.set_page_config(page_title="뭐든지 질문하세요!")
@@ -10,14 +10,13 @@ load_dotenv()
 
 import os
 api_key = os.getenv("UPSTAGE_API_KEY")
-llm = ChatUpstage(model_name="solar-pro", upstage_api_key=api_key, upstage_api_base="https://api.upstage.ai/v1/solar", temperature=0)
+llm = SolarChat(model="solar-1-mini-chat", solar_api_key=api_key)
 
 def generate_response(input_text):
     message = [HumanMessage(content=input_text)]  # ✅ HumanMessage 객체로 Wrapping
     response = llm(message)  # ✅ 올바른 형식으로 전달
 
-    st.info(response)
-    # st.info(llm(input_text))
+    st.info(response.content)
 
 with st.form('Question'):
     text = st.text_area('질문 입력:', 'What types of text models does OpenAI provide?')
