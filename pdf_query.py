984eda6b4c7a5009da10f8277b1517458975381b
WorkaHolic <baileypark@kakao.com>
2025-02-24 00:12:45 +0900

    feat: RAG를 활용한 독립형 질문 챗봇 만들기

--- pdf_query.py
+++ pdf_query.py
@@ -1,64 +1,75 @@
 import os
 import streamlit as st
-from streamlit_chat import message
+from PyPDF2 import PdfReader
+from gitdb.fun import chunk_size
 from langchain_community.embeddings import SentenceTransformerEmbeddings
 from langchain_community.chat_models.solar import SolarChat
-from langchain.chains.question_answering import load_qa_chain
-from langchain.chains import ConversationalRetrievalChain
+from langchain.chains import ConversationalRetrievalChain, RetrievalQA
+from langchain.memory import ConversationBufferWindowMemory
 from langchain.vectorstores import FAISS
-import tempfile
 from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
 # .env 파일 로드
 load_dotenv()
 api_key = os.getenv("UPSTAGE_API_KEY")
 
-uploaded_file = st.sidebar.file_uploader("upload", type="pdf")
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
-if uploaded_file :
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        tmp_file.write(uploaded_file.getvalue())
-        tmp_file_path = tmp_file.name
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators="\\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
 
-    loader = PyPDFLoader(tmp_file_path)
-    data = loader.load()
-
+def get_vertorstore(text_chunks):
     embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-    vectors = FAISS.from_documents(data, embeddings)
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
 
-    chain = ConversationalRetrievalChain.from_llm(llm = SolarChat(model="solar-1-mini-chat", solar_api_key=api_key), retriever=vectors.as_retriever())
+def get_conversation_chain(vectorstore):
+    memory = ConversationBufferWindowMemory(memory_key='chat_history', return_message=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm = SolarChat(temperature=0, model="solar-1-mini-chat", solar_api_key=api_key),
+        retriever=vectorstore.as_retriever(),
+        get_chat_history=lambda h: h,
+        memory=memory
+    ) #ConversationalRetrievalChain을 통해 langchain 챗봇에 퀀리 전송
+    return conversation_chain
 
-    def conversational_chat(query):
-        result = chain({"question": query, "chat_history": st.session_state['history']})
-        st.session_state['history'].append((query, result["answer"]))
-        return result["answer"]
+user_uploads = st.file_uploader("파일을 업로드해주세요~", accept_multiple_files=True)
+if user_uploads is not None:
+    if st.button("Upload"):
+        with st.spinner("처리중.."):
+            # PDF 텍스트 가져오기
+            raw_text = get_pdf_text(user_uploads)
+            # 텍스트에서 청크 검색
+            text_chunks = get_text_chunks(raw_text)
+            # PDF 텍스트 저장을 위해 FAISS 벡터 저장소 만들기
+            vertorstore = get_vertorstore(text_chunks)
+            # 대화 체인 만들기
+            st.session_state.conversation = get_conversation_chain(vertorstore)
 
-    if 'history' not in st.session_state:
-        st.session_state['history'] = []
+if user_query := st.chat_input("질문을 입력해주세요~"):
+    # 대화 체인을 사용하여 사용자의 메시지를 처리
+    if 'conversation' in st.session_state:
+        result = st.session_state.conversation({
+            "question": user_query,
+            "chat_history": st.session_state.get('chat_history', [])
+        })
+        response = result["answer"]
+    else:
+        response = "먼저 문서를 업로드해주세요~."
+    with st.chat_message("assistant"):
+        st.write(response)
 
-    if 'generated' not in st.session_state:
-        st.session_state['generated'] = ["안녕하세요! " + uploaded_file.name + "에 관해 질문주세요."]
-
-    if 'past' not in st.session_state:
-        st.session_state['past'] = ["안녕하세요!"]
-
-    # 챗봇 이력에 대한 컨테이너
-    response_container = st.container()
-    container = st.container()
-
-    with container:
-        with st.form(key='Conv_Question', clear_on_submit=True):
-            user_input = st.text_input("Query:", placeholder="PDF파일에 대해 얘기해볼까요? (:", key='input')
-            submit_button = st.form_submit_button(label='Send')
-
-        if submit_button and user_input:
-            output = conversational_chat(user_input)
-
-            st.session_state['past'].append(user_input)
-            st.session_state['generated'].append(output)
-
-    if st.session_state['generated']:
-        with response_container:
-            for i in range(len(st.session_state['generated'])):
-                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style = "fun-emoji", seed = "Nala")
-                message(st.session_state["generated"][i], key=str(i), avatar_style = "bottts", seed = "Fluffy")
